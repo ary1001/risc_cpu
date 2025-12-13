d6dc01de73d7b61553028d82d7674da7a5a1d59f
ary1001 <een242857@ee.iitd.ac.in>
2025-12-13 18:48:04 +0530

    Update alu.sv

--- RTL/1_RTL_code/alu.sv
+++ RTL/1_RTL_code/alu.sv
@@ -1,31 +1,37 @@
-
 module alu
-  import typedefs::*;  
- (
+  import typedefs::*;
+(
   output logic [7:0] out,
-  output logic       zero,       //carry, zero flag
-  input  logic [7:0]  accum,
-  input  logic [7:0]  data,
+  output logic       zero,     // zero flag
+  input  logic [7:0] accum,
+  input  logic [7:0] data,
   input  opcode_t    opcode,
-  input  logic        clk 
- );
+  input  logic       clk
+);
 
+  // -------------------------------------------------
+  // ALU operation (negative-edge triggered)
+  // -------------------------------------------------
+  always_ff @(negedge clk) begin
+    unique case (opcode)
+      HLT : out <= accum;
+      SKZ : out <= accum;
+      ADD : out <= accum + data;
+      AND : out <= accum & data;
+      XOR : out <= accum ^ data;
+      LDA : out <= data;
+      STO : out <= accum;
+      JMP : out <= accum;
+    endcase
+  end
 
-  always_ff @(negedge clk)
-     begin
-        unique case(opcode)
-        HLT : out <=   accum;
-        SKZ : out <=   accum;
-        ADD : out <=   accum + data;
-        AND : out <=   accum & data;
-        XOR : out <=   accum ^ data;
-        LDA : out <=   data;
-        STO : out <=   accum;
-        JMP : out <=   accum; 
-        endcase 
-     end
-
- always_comb
-  zero = (accum == '0);
+  // -------------------------------------------------
+  // Zero flag generation
+  // -------------------------------------------------
+  always_comb begin
+    zero = (accum == '0);
+  end
 
 endmodule : alu
+
+
